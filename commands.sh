d743b2ff5952ae7d3feec3c6b786b0424de7fda8
maxim.kozlyuk@yandex.ru <vypzyx-6mAkbi-qyvdit>
2020-02-23 15:52:50 +0400

    lab 3 done

--- commands.sh
+++ commands.sh
@@ -1,3 +1,9 @@
+sudo ssh -p 2022 root@127.0.0.1
+
+export CFLAGS=`xeno-config --xeno-cflags`
+export LDFLAGS=`xeno-config --xeno-ldflags`
+export LD_LIBRARY_PATH=/usr/xenomai/lib
+
 sudo scp -P 2022 <local_file_name> root@127.0.0.1:/root/<user_folder>
 
 gcc $CFLAGS $LDFLAGS -lnative -lrtdk <c_source_name> -o <result_name>
