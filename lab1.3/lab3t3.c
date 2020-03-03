aa5659369446160275550a75a0b67d4ae16422d0
maxim.kozlyuk@yandex.ru <vypzyx-6mAkbi-qyvdit>
2020-03-03 10:39:27 +0400

    rename

--- lab1.3/lab3t3.c
+++ lab1.3/lab3t3.c
@@ -15,7 +15,6 @@
 #define SEM_MODE S_FIFO
 
 RT_SEM sem1;
-RT_SEM sem2;
 
 static RT_TASK t1;
 static RT_TASK t2;
@@ -23,21 +22,24 @@ static RT_TASK t3;
 
 int global = 0;
 
+/**
+ * rt_sem_p - Уменьшение счетчика семафора, если счетчик = 0 то включается блокировка процесса
+ * rt_sem_v(&sem_desc); - Увеличение счетчика семафора
+ */
+
 void taskOne(void *arg) {
     rt_sem_p(&sem1, TM_INFINITE);
-
     int i;
     for (i = 0; i < ITER; i++) {
-        rt_printf("I am taskOne and global=%d 1 1 1\n", ++global);
+        rt_printf("I am taskOne and global=%d 1\n", ++global);
     }
 }
 
 void taskTwo(void *arg) {
-    rt_sem_p(&sem2, TM_INFINITE);
-
+    rt_sem_p(&sem1, TM_INFINITE);
     int i;
     for (i = 0; i < ITER; i++) {
-        rt_printf("I am taskTwo and global=%d 2 2 2\n", -global);
+        rt_printf("I am taskTwo and global=%d 2\n", -global);
     }
 
     rt_sem_v(&sem1);
@@ -46,7 +48,7 @@ void taskTwo(void *arg) {
 void taskThree(void *arg) {
     int i;
     for (i = 0; i < ITER; i++) {
-        rt_printf("I am taskThree and global=%d 3 3 3\n", ++global);
+        rt_printf("I am taskThree and global=%d 3\n", ++global);
     }
     rt_sem_v(&sem2);
 }
@@ -57,7 +59,6 @@ int main(int argc, char *argv[]) {
     mlockall(MCL_CURRENT | MCL_FUTURE);
 
     rt_sem_create(&sem1, "MySemaphore", SEM_INIT, SEM_MODE);
-    rt_sem_create(&sem2, "Sem2", SEM_INIT, SEM_MODE);
 
     rt_task_create(&t1, "task1", 0, 1, 0);
     rt_task_create(&t2, "task2", 0, 1, 0);
